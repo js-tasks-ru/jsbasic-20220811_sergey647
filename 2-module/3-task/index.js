50cf036d06c86fb55383989a81e0cd24085a6818
bogdanovsa <bogdanovsa1@edu.mos.ru>
2022-08-12 15:19:04 +0300

    2-module/*

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,16 @@
 let calculator = {
-  // ваш код
+  a: 0,
+  b: 0,
+  read: function (a, b) {
+    window.calculator.a = a;
+    window.calculator.b = b;
+  },
+  sum: function () {
+    return this.a + this.b;
+  },
+  mul: function () {
+    return this.a * this.b;
+  },
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
