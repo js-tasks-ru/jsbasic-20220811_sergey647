9d41464344edf1e5807e0d601f8048c38b520b4d
bogdanovsa <bogdanovsa1@edu.mos.ru>
2022-08-15 19:16:11 +0300

    2-module/3-task [FIX]

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -2,8 +2,8 @@ let calculator = {
   a: 0,
   b: 0,
   read: function (a, b) {
-    window.calculator.a = a;
-    window.calculator.b = b;
+    this.a = a;
+    this.b = b;
   },
   sum: function () {
     return this.a + this.b;
